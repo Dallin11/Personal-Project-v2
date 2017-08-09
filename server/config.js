162e9e21301dd8f7d7abec8de14b58c1e2c4f684
Dallin <andersondallin11@gmail.com>
2017-08-09 15:24:29 -0600

    changed database url

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    database:"postgres://kqqfdvei:aTN2vjbUAi3kd7md_1p3ko3NH_v9c66w@stampy.db.elephantsql.com:5432/kqqfdvei",
+    database:"postgres://kqqfdvei:bmnPzrL2wJZYjbw5nGoea_80WOvBMM1L@stampy.db.elephantsql.com:5432/kqqfdvei",
      domain: "dallin.auth0.com",
     clientID: "rtJ6AAQMDP9QUwlJW0Uy4Eih2Dv3Glal",
     clientSecret: "2fWBch27WLbED314kepUyhDs_l_JTNrDQWei9MOv19gBslr79pmdMqksdCE2-JRJ",
